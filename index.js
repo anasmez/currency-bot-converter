008987cac1a0af9a43f0f98c955ad79270e1dfc9
Anas M <anasmez@outlook.es>
2022-10-01 03:46:22 +0200

    Muestra cambio en estado, nombre y mensaje de perfil

--- index.js
+++ index.js
@@ -1,18 +1,79 @@
+const fetch = require('node-fetch');
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 require('dotenv').config();
 
-const token = process.env.DISCORD_TOKEN;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const CURRENCY_API_URL = 'https://api.coinbase.com/v2/exchange-rates?currency=EUR';
+const UPDATE_INTERVAL = process.env.INTERVAL_TIME || 5 * 1000;
 
-if (!token) throw new Error('No token provided');
+const config = {
+	LAST_EUR_USD_RATE: undefined,
+};
+const botPresenceData = {
+	status: 'online',
+	activities: [{
+		name: 'EUR-USD: ',
+		type: ActivityType.Watching,
+	}],
+	afk: false,
+};
 
-// Create a new client instance
+if (!DISCORD_TOKEN) throw new Error('No token provided');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+	updateBot();
+	setInterval(updateBot, UPDATE_INTERVAL);
 });
 
-// Login to Discord with your client's token
-client.login(token);
+client.login(DISCORD_TOKEN);
+
+async function getPrice() {
+	try {
+		const response = await fetch(CURRENCY_API_URL);
+		const { data } = (await response.json());
+		return data?.rates?.USD;
+	} catch (error) {
+		console.error('Failed to fetch', error);
+	}
+}
+
+function formatMoneyString(string) {
+	const decimalIndex = string.search('.');
+	if (decimalIndex == -1) {
+		return '$--.--';
+	}
+	return `$${string.substring(0, 6)}`;
+}
+
+function updateBotStatusMessage(status) {
+	botPresenceData.activities[0].name = status;
+	client.user.setPresence(botPresenceData);
+}
+
+function updateBotNickname(name) {
+	for (const [, guild] of client.guilds.cache) {
+		guild.members.me.setNickname(name);
+	}
+}
+
+async function updateBot() {
+	let newPrice = await getPrice();
+	if (!newPrice) return;
+	let emoji = '-';
+	let percentage;
+	if (config.LAST_EUR_USD_RATE && newPrice !== config.LAST_EUR_USD_RATE) {
+		percentage = newPrice / config.LAST_EUR_USD_RATE;
+		emoji = percentage > 0 ? '↗' : '↘';
+		config.LAST_EUR_USD_RATE = newPrice;
+	}
+	newPrice = formatMoneyString(newPrice);
+	const statusMessage = `%${ percentage }`.substring(0, 5);
+	const nickname = `${ newPrice } (${ emoji })`;
+	updateBotStatusMessage(statusMessage);
+	updateBotNickname(nickname);
+	console.log(`Activity set to ${ nickname }`);
+}
